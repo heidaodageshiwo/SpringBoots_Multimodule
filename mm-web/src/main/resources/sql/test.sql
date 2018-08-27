f6d1b197b7fe232571609de97ce942c5c53f4604
zhangqiang <heidaodageshiwo@163.com>
2018-08-27 16:31:47 +0800

    sql

---
+++ mm-web/src/main/resources/sql/test.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.7.20-log : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `test`;
+
+/*Table structure for table `t_usertest` */
+
+DROP TABLE IF EXISTS `t_usertest`;
+
+CREATE TABLE `t_usertest` (
+  `userid` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `username` varchar(30) DEFAULT NULL COMMENT '名称',
+  `password` int(10) DEFAULT NULL COMMENT '年龄',
+  PRIMARY KEY (`userid`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+/*Data for the table `t_usertest` */
+
+insert  into `t_usertest`(`userid`,`username`,`password`) values (1,'2',3),(4,'5',6),(7,'8',44444);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
